d96f8d86f3e2491ab10d323b5802e84834da6414
MaryLena Bleile <mbleile@smu.edu>
2024-11-01 16:58:21 -0400

    Update make_anndata_from_seurat_clean.R

--- scripts/make_anndata_from_seurat_clean.R
+++ scripts/make_anndata_from_seurat_clean.R
@@ -5,38 +5,29 @@ library(Seurat)
 
 sc <- import("scanpy", convert = FALSE)
 
-
-load("Github Data/PPD1_22")
-load("Github Data/cleanreference")
+sampleid="PPD1_22"
+load(paste("H:/Biostatistics/PICI/data/ST/sct/", sampleid, sep=""))
+load("H:/Biostatistics/PICI/data/ST/SmithyMS/ChrisMarineData/data/cleanreference")
 
 adata <- convertFormat(spat, from="seurat", to="anndata", main_layer="counts", assay="Spatial",drop_single_values=FALSE)
 adata
 
 write_h5ad(
   anndata=adata,
-  filename = "Github Data/PPD1_22.h5ad",
-  compression = NULL,
-  compression_opts = NULL,
-  as_dense = list()
-)
-scdata <- convertFormat(cref, from="seurat", to="anndata", main_layer="counts", assay="RNA",drop_single_values=FALSE)
-
-
-write_h5ad(
-  anndata=scdata,
-  filename = "Github Data/reference.h5ad",
+  filename = "H:/Biostatistics/PICI/data/ST/BleileMS/data/PPD1_22.h5ad",
   compression = NULL,
   compression_opts = NULL,
   as_dense = list()
 )
 
-desired.types = c("Neural_Crest_like","Melanocytic","Immune","Stroma")
+
+desired.types = c("Neural_Crest_like","Melanocytic","Mesenchymal_like","Immune","Stroma")
 cref.subset <- subset(cref, subset= Cluster %in% desired.types)
 
 scdata2 <- convertFormat(cref.subset, from="seurat", to="anndata", main_layer="counts", assay="RNA",drop_single_values=FALSE)
 write_h5ad(
   anndata=scdata2,
-  filename = "Github Data/reference.subset.h5ad",
+  filename = "H:/Biostatistics/PICI/data/ST/BleileMS/data/reference_6type.h5ad",
   compression = NULL,
   compression_opts = NULL,
   as_dense = list()
