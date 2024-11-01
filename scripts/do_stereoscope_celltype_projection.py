7e9efc879dc4fb754780d0f068e2eb5bd07457ef
MaryLena Bleile <mbleile@smu.edu>
2024-11-01 10:13:23 -0400

    Update do_stereoscope_celltype_projection.py
    update the code for implementation of stereoscope

--- scripts/do_stereoscope_celltype_projection.py
+++ scripts/do_stereoscope_celltype_projection.py
@@ -4,12 +4,14 @@ import matplotlib.pyplot as plt
 import anndata
 import muon
 import numpy as np
-#import pooch
+from ray import tune
+import ray
+from scvi import autotune
 import scanpy as sc
 import scvi
 import seaborn as sns
 import torch
-from scvi.model import CondSCVI, DestVI
+from scvi.external import RNAStereoscope, SpatialStereoscope
 
 sc.set_figure_params(figsize=(6, 6), frameon=False)
 sns.set_theme()
@@ -20,14 +22,14 @@ save_dir = tempfile.TemporaryDirectory()
 st_adata = sc.read(filename="H:/Biostatistics/PICI/data/ST/BleileMS/data/PPD1_22.h5ad")
 st_adata.var_names_make_unique()
 
-sc_adata = sc.read(filename="H:/Biostatistics/PICI/data/ST/BleileMS/data/reference.mel.reduced.h5ad")
+sc_adata = sc.read(filename="H:/Biostatistics/PICI/data/ST/BleileMS/data/reference_6type.h5ad")
 sc_adata.var_names_make_unique()
 
 
 print("# cells, # genes before filtering:", sc_adata.shape)
 
 # let us filter some genes
-G = 1000
+G = 2000
 sc.pp.filter_genes(sc_adata, min_counts=10)
 
 sc_adata.layers["counts"] = sc_adata.X.copy()
@@ -53,25 +55,35 @@ G = len(intersect)
 #sc.pl.embedding(st_adata, basis="spatial", color="seurat_clusters", s=80)
 
 
-CondSCVI.setup_anndata(sc_adata, layer="counts", labels_key="Cluster")
+RNAStereoscope.setup_anndata(sc_adata, layer="counts", labels_key="Cluster")
 
-sc_model = CondSCVI(sc_adata, weight_obs=False)
-sc_model.view_anndata_setup()
+train = False
+if train:
+    sc_model = RNAStereoscope(sc_adata)
+    sc_model.train(max_epochs=400)
+    sc_model.history["elbo_train"][10:].plot()
+    sc_model.save("scmodel400ep", overwrite=True)
+else:
+    sc_model = RNAStereoscope.load("scmodel400ep", adata=sc_adata)
+    print("Loaded RNA model from file!")
 
-sc_model.train()
+#plt.savefig("H:/Biostatistics/PICI/data/ST/BleileMS/data/stereoscope_sc_model_400ep.png")
 
-sc_model.history["elbo_train"].iloc[5:].plot()
-#plt.show()
+SpatialStereoscope.setup_anndata(st_adata, layer="counts")
+train = True
+if train:
+    #SpatialStereoscope.load("stmodel_400sc_1250ep", overwrite=True)
+    spatial_model = SpatialStereoscope.from_rna_model(st_adata, sc_model)
+    spatial_model.train(max_epochs=2000)
+    spatial_model.history["elbo_train"][10:].plot()
+    spatial_model.save("stmodel_6type_400sc_2000ep", overwrite=True)
+else:
+    spatial_model = SpatialStereoscope.load("stmodel", adata=st_adata)
+    print("Loaded Spatial model from file!")
 
-DestVI.setup_anndata(st_adata, layer="counts")
 
-st_model = DestVI.from_rna_model(st_adata, sc_model)
-st_model.view_anndata_setup()
+plt.savefig("H:/Biostatistics/PICI/data/ST/BleileMS/data/stereoscope_st_model_6type_st_2000_sc_400.png")
 
-st_model.train(max_epochs=2500)
-st_model.history["elbo_train"].iloc[10:].plot()
-plt.show()
+st_adata.obsm["proportions"] = spatial_model.get_proportions()
 
-st_adata.obsm["proportions"] = st_model.get_proportions()
-
-st_adata.obsm["proportions"].to_csv("H:/Biostatistics/PICI/data/ST/BleileMS/data/stereo_projections_mel_reduced.csv")
+st_adata.obsm["proportions"].to_csv("H:/Biostatistics/PICI/data/ST/BleileMS/data/stereoscope_6type_400sc_2000st.csv")
