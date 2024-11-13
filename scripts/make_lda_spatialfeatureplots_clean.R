53e8cec4397cb6f63c15b7da2e208fef8d32c2c0
MaryLena Bleile <mbleile@smu.edu>
2024-11-13 15:49:22 -0500

    Update make_lda_spatialfeatureplots_clean.R

--- scripts/make_lda_spatialfeatureplots_clean.R
+++ scripts/make_lda_spatialfeatureplots_clean.R
@@ -1,23 +1,20 @@
 library(Seurat)
 library(ScSpatialFeaturePlot)
 load("data/VisiumData.rda")
-load("data/ldas.rda")
+load("results/deconProp_K8.rda")
 
 
-K=8
-optLDA = ldas$models[[K-4]]
-results <- getBetaTheta(optLDA, perc.filt = 0.05, betaScale = 1)
-deconProp <- results$theta
-colnames(deconProp) = paste("LDA_Factor",colnames(deconProp), sep="")
-deconGexp <- results$beta
-#colnames(pos) = c("y","x")
+
 predictions=colnames(deconProp)[apply(deconProp,1, which.max)]
 names(predictions)=rownames(deconProp)
 spat$LDA.predicted.id = predictions
 
+SpatialDimPlot(spat, group.by="LDA.predicted.id",)
 
-spat@meta.data =cbind(spat@meta.data[rownames(deconProp),], deconProp)
-
+for(one.feature in colnames(deconProp)){
+    spat[[one.feature]] = deconProp[,one.feature]
+}
 
 ScSpatialFeaturePlot(spat, uq=.99, lq=.00,LegLabel = "Proportion",
                      features=colnames(deconProp), flip=3)
+
